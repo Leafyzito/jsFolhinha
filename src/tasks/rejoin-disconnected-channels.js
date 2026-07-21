3035dcdf0902c5de4d53e1146fc0295cdccfac45
leafy <rafaelzao2003@gmail.com>
2026-07-21 21:27:34 +0100

    chore(tasks): remove state updating

--- src/tasks/rejoin-disconnected-channels.js
+++ src/tasks/rejoin-disconnected-channels.js
@@ -1,10 +1,10 @@
 async function rejoinDisconnectedChannels() {
-  const normalizeLogin = (s) =>
-    (s ?? "").toString().trim().replace(/^#/, "").toLowerCase();
+  // const normalizeLogin = (s) =>
+  //   (s ?? "").toString().trim().replace(/^#/, "").toLowerCase();
 
-  const connectedChannels = new Set(
-    [...fb.twitch.anonClient.currentChannels].map(normalizeLogin)
-  );
+  // const connectedChannels = new Set(
+  //   [...fb.twitch.anonClient.currentChannels].map(normalizeLogin)
+  // );
 
   // fetch channels to join from config table
   const channelsToJoin = await fb.db.get("config", { state: "active" });
@@ -14,23 +14,23 @@ async function rejoinDisconnectedChannels() {
     (channel) => channel.channel
   );
 
-  for (const channel of channelsToJoin) {
-    const channelLogin = normalizeLogin(channel.channel);
-    if (!connectedChannels.has(channelLogin)) {
-      // verify user exists in twitch
-      const user = await fb.api.helix.getUserByUsername(channelLogin);
-      if (!user) {
-        await fb.db.update(
-          "config",
-          { channelId: channel.channelId },
-          { $set: { state: "inactive" } }
-        );
-        continue;
-      }
+  // for (const channel of channelsToJoin) {
+  //   const channelLogin = normalizeLogin(channel.channel);
+  //   if (!connectedChannels.has(channelLogin)) {
+  //     // verify user exists in twitch
+  //     const user = await fb.api.helix.getUserByUsername(channelLogin);
+  //     if (!user) {
+  //       await fb.db.update(
+  //         "config",
+  //         { channelId: channel.channelId },
+  //         { $set: { state: "inactive" } }
+  //       );
+  //       continue;
+  //     }
 
-      fb.twitch.anonClient.join(channelLogin);
-    }
-  }
+  //     fb.twitch.anonClient.join(channelLogin);
+  //   }
+  // }
 }
 
 module.exports = rejoinDisconnectedChannels;
