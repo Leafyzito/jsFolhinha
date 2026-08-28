27341abd316b793996394691ef7abae28833c61b
leafy <rafaelzao2003@gmail.com>
2026-08-28 04:11:33 +0100

    chore: disable chatters and checknick

--- src/commands/chatters/chatters.js
+++ src/commands/chatters/chatters.js
@@ -1,86 +1,86 @@
-const path = require("path");
-async function getChatters(channel) {
-  const api_url = "https://api.fuchsty.de/twitch/chatters/" + channel;
-  const response = await fb.got(api_url);
+// const path = require("path");
+// async function getChatters(channel) {
+//   const api_url = "https://api.fuchsty.de/twitch/chatters/" + channel;
+//   const response = await fb.got(api_url);
 
-  if (!response) {
-    return "erro";
-  }
+//   if (!response) {
+//     return "erro";
+//   }
 
-  const data = response;
+//   const data = response;
 
-  if ("error" in data) {
-    return "erro";
-  }
+//   if ("error" in data) {
+//     return "erro";
+//   }
 
-  const count = data.count;
-  if (count === 0) {
-    return null;
-  }
+//   const count = data.count;
+//   if (count === 0) {
+//     return null;
+//   }
 
-  const broadcaster = data.chatters.broadcasters || [];
-  const mods = data.chatters.moderators || [];
-  const vips = data.chatters.vips || [];
-  const viewers = data.chatters.viewers || [];
+//   const broadcaster = data.chatters.broadcasters || [];
+//   const mods = data.chatters.moderators || [];
+//   const vips = data.chatters.vips || [];
+//   const viewers = data.chatters.viewers || [];
 
-  return [broadcaster, mods, vips, viewers, count];
-}
+//   return [broadcaster, mods, vips, viewers, count];
+// }
 
-const chattersCommand = async (message) => {
-  const targetChannel =
-    message.args[1]?.replace(/^@/, "") || message.channelName;
-  const chattersRes = await getChatters(targetChannel);
+// const chattersCommand = async (message) => {
+//   const targetChannel =
+//     message.args[1]?.replace(/^@/, "") || message.channelName;
+//   const chattersRes = await getChatters(targetChannel);
 
-  if (chattersRes === "erro") {
-    return {
-      reply: `Esse usuário não existe`,
-    };
-  }
+//   if (chattersRes === "erro") {
+//     return {
+//       reply: `Esse usuário não existe`,
+//     };
+//   }
 
-  if (!chattersRes) {
-    return {
-      reply: `Não há chatters em ${targetChannel}`,
-    };
-  }
+//   if (!chattersRes) {
+//     return {
+//       reply: `Não há chatters em ${targetChannel}`,
+//     };
+//   }
 
-  let streamer = chattersRes[0];
-  let mods = chattersRes[1];
-  let vips = chattersRes[2];
-  let viewers = chattersRes[3];
-  const count = chattersRes[4];
+//   let streamer = chattersRes[0];
+//   let mods = chattersRes[1];
+//   let vips = chattersRes[2];
+//   let viewers = chattersRes[3];
+//   const count = chattersRes[4];
 
-  streamer = streamer.join("\n");
-  mods = mods.sort().join("\n");
-  vips = vips.sort().join("\n");
-  viewers = viewers.sort().join("\n");
+//   streamer = streamer.join("\n");
+//   mods = mods.sort().join("\n");
+//   vips = vips.sort().join("\n");
+//   viewers = viewers.sort().join("\n");
 
-  const finalList = `Total de chatters em #${targetChannel}: ${count}\n\nStreamer:\n${streamer}\n\nModeradores: (${mods.length})\n${mods}\n\nVips: (${vips.length})\n${vips}\n\nChatters: (${viewers.length})\n${viewers}`;
+//   const finalList = `Total de chatters em #${targetChannel}: ${count}\n\nStreamer:\n${streamer}\n\nModeradores: (${mods.length})\n${mods}\n\nVips: (${vips.length})\n${vips}\n\nChatters: (${viewers.length})\n${viewers}`;
 
-  const gistUrl = await fb.api.github.createGist(finalList);
+//   const gistUrl = await fb.api.github.createGist(finalList);
 
-  if (count > 99) {
-    return {
-      reply: `Existem ${count} chatters em #${targetChannel}: ${gistUrl} (devido a limitações da Twitch, esta lista contém apenas 100 chatters)`,
-    };
-  }
+//   if (count > 99) {
+//     return {
+//       reply: `Existem ${count} chatters em #${targetChannel}: ${gistUrl} (devido a limitações da Twitch, esta lista contém apenas 100 chatters)`,
+//     };
+//   }
 
-  return {
-    reply: `Existem ${count} chatters em #${targetChannel}: ${gistUrl}`,
-  };
-};
+//   return {
+//     reply: `Existem ${count} chatters em #${targetChannel}: ${gistUrl}`,
+//   };
+// };
 
-chattersCommand.commandName = "chatters";
-chattersCommand.aliases = ["chatters"];
-chattersCommand.shortDescription = "Mostra a lista de chatters de algum canal";
-chattersCommand.cooldown = 5000;
-chattersCommand.cooldownType = "channel";
-chattersCommand.whisperable = false;
-chattersCommand.description = `Exibe uma lista de usuários totais online no canal e suas devidas categorias (Streamer, Moderadores, VIPs, Chatters)
-O comando funcionará mesmo em canais que o Folhinha não esteja presente
-• Exemplo: !chatters - Exibe a lista de chatters do canal atual
-• Exemplo: !chatters {canal} - Exibe a lista de chatters do canal escolhido`;
-chattersCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/src/commands/${__dirname.split(path.sep).pop()}/${__filename.split(path.sep).pop()}`;
+// chattersCommand.commandName = "chatters";
+// chattersCommand.aliases = ["chatters"];
+// chattersCommand.shortDescription = "Mostra a lista de chatters de algum canal";
+// chattersCommand.cooldown = 5000;
+// chattersCommand.cooldownType = "channel";
+// chattersCommand.whisperable = false;
+// chattersCommand.description = `Exibe uma lista de usuários totais online no canal e suas devidas categorias (Streamer, Moderadores, VIPs, Chatters)
+// O comando funcionará mesmo em canais que o Folhinha não esteja presente
+// • Exemplo: !chatters - Exibe a lista de chatters do canal atual
+// • Exemplo: !chatters {canal} - Exibe a lista de chatters do canal escolhido`;
+// chattersCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/src/commands/${__dirname.split(path.sep).pop()}/${__filename.split(path.sep).pop()}`;
 
-module.exports = {
-  chattersCommand,
-};
+// module.exports = {
+//   chattersCommand,
+// };
--- src/commands/checknick/checknick.js
+++ src/commands/checknick/checknick.js
@@ -1,76 +1,76 @@
-const path = require("path");
-async function checkNick(nick) {
-  const api_url = `https://api.fuchsty.de/twitch/checkname?username=${nick}`;
-  const response = await fb.got(api_url);
+// const path = require("path");
+// async function checkNick(nick) {
+//   const api_url = `https://api.fuchsty.de/twitch/checkname?username=${nick}`;
+//   const response = await fb.got(api_url);
 
-  if (!response) {
-    return "invalid";
-  }
+//   if (!response) {
+//     return "invalid";
+//   }
 
-  const data = response;
+//   const data = response;
 
-  const isInvalid = data[0].invalid;
-  if (isInvalid) {
-    return "invalid";
-  }
+//   const isInvalid = data[0].invalid;
+//   if (isInvalid) {
+//     return "invalid";
+//   }
 
-  const isAvailable = data[0].available;
-  if (isAvailable) {
-    return true;
-  }
+//   const isAvailable = data[0].available;
+//   if (isAvailable) {
+//     return true;
+//   }
 
-  return false;
-}
+//   return false;
+// }
 
-const checkNickCommand = async (message) => {
-  if (message.args.length < 2) {
-    return {
-      reply: `Use o formato: ${message.prefix}checknick <nick>`,
-    };
-  }
+// const checkNickCommand = async (message) => {
+//   if (message.args.length < 2) {
+//     return {
+//       reply: `Use o formato: ${message.prefix}checknick <nick>`,
+//     };
+//   }
 
-  const nick = message.args[1].replace(/^@/, "");
+//   const nick = message.args[1].replace(/^@/, "");
 
-  const checkNickRes = await checkNick(nick);
+//   const checkNickRes = await checkNick(nick);
 
-  if (checkNickRes === "invalid") {
-    return {
-      reply: `O nick ${nick} é inválido`,
-    };
-  }
+//   if (checkNickRes === "invalid") {
+//     return {
+//       reply: `O nick ${nick} é inválido`,
+//     };
+//   }
 
-  if (!checkNickRes) {
-    const emote = await fb.emotes.getEmoteFromList(
-      message.channelName,
-      ["paia"],
-      "👎"
-    );
-    return {
-      reply: `O nick ${nick} não está disponível ${emote}`,
-    };
-  }
+//   if (!checkNickRes) {
+//     const emote = await fb.emotes.getEmoteFromList(
+//       message.channelName,
+//       ["paia"],
+//       "👎"
+//     );
+//     return {
+//       reply: `O nick ${nick} não está disponível ${emote}`,
+//     };
+//   }
 
-  const emote = await fb.emotes.getEmoteFromList(
-    message.channelName,
-    ["joia", "jumilhao"],
-    "👍"
-  );
-  return {
-    reply: `O nick ${nick} está disponível ${emote}`,
-  };
-};
+//   const emote = await fb.emotes.getEmoteFromList(
+//     message.channelName,
+//     ["joia", "jumilhao"],
+//     "👍"
+//   );
+//   return {
+//     reply: `O nick ${nick} está disponível ${emote}`,
+//   };
+// };
 
-checkNickCommand.commandName = "checknick";
-checkNickCommand.aliases = ["checknick", "nickcheck", "namecheck", "checkname"];
-checkNickCommand.shortDescription =
-  "Verifica se um nick específico está disponível";
-checkNickCommand.cooldown = 5000;
-checkNickCommand.cooldownType = "channel";
-checkNickCommand.whisperable = true;
-checkNickCommand.description = `Use este comando para ver se um nick específico é válido e está disponível ou não
-• Exemplo: !checknick leafyzito - Verifica se o nick "leafyzito" está disponível`;
-checkNickCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/src/commands/${__dirname.split(path.sep).pop()}/${__filename.split(path.sep).pop()}`;
+// checkNickCommand.commandName = "checknick";
+// checkNickCommand.aliases = ["checknick", "nickcheck", "namecheck", "checkname"];
+// checkNickCommand.shortDescription =
+//   "Verifica se um nick específico está disponível";
+// checkNickCommand.cooldown = 5000;
+// checkNickCommand.cooldownType = "channel";
+// checkNickCommand.whisperable = true;
+// checkNickCommand.description = `Use este comando para ver se um nick específico é válido e está disponível ou não
+// • Exemplo: !checknick leafyzito - Verifica se o nick "leafyzito" está disponível`;
+// checkNickCommand.code = `https://github.com/leafyzito/jsFolhinha/blob/main/src/commands/${__dirname.split(path.sep).pop()}/${__filename.split(path.sep).pop()}`;
 
-module.exports = {
-  checkNickCommand,
-};
+// module.exports = {
+//   checkNickCommand,
+// };
